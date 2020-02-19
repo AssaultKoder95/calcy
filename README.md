8ab0be2adf2e7d039b5acea4025d27c00e5a4900
Abhishek Khanna <abhishek@imagekit.io>
2020-02-19 08:42:58 +0530

    Basic app setup

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# calcy
-Same old calculator made in REACT
+# Calcy - The Ultimate Calculator
+A demo react app of our most famous web applications - Calculator
+
+Current state of demo contains:
+
+-  Basic Calculations
+
+## Basic Structure
+The component structure of the app is simple, we have a main component called `App` inside which our components reside.
+
+### Component STRUCTURE:
+
+- APP
+  - Header
+  - Calculator
+    - Result UI
+    - Number pattern display UI
+    - Operations pattern display UI
+  - Footer
+
+## Future
+The list of features that I might develop in future:
+
+- Advance Calculations
+- Series generator (AP, GP, Fibonacci etc.)
+- Finding specific elements of a given series 
--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
+import React from 'react'
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SimpleCalculator from './components/SimpleCalculator';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () => {
+    return (
+        <div className="App">
+            <Header />
+            <SimpleCalculator />
+            <Footer />
+        </div>
+    );
 }
 
 export default App;
