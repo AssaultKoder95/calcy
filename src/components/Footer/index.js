f166fc6050a8c1e548b629c87cb3ba6ddc348704
Abhishek Khanna <abhishek@imagekit.io>
2020-02-19 08:43:10 +0530

    footer module

---
+++ src/components/Footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="Footer">
+            <h5>Made with Heart - AssaultKoder95 </h5>
+        </div>
+    );
+};
+
+export default Footer;
